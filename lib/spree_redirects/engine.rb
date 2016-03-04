30620e0ec40f28e29f17042abe08795e61f9a3cd
Warren Sangster <warren@moneyspyder.co.uk>
2016-03-04 10:08:09 +0000

    HH-165 Temporarily switching off the intercept

--- lib/spree_redirects/engine.rb
+++ lib/spree_redirects/engine.rb
@@ -2,9 +2,9 @@ module SpreeRedirects
   class Engine < Rails::Engine
     engine_name "spree_redirects"
     
-    initializer "redirect middleware" do |app|
-      app.middleware.insert_after ::ActionDispatch::DebugExceptions, ::SpreeRedirects::RedirectMiddleware
-    end
+    # initializer "redirect middleware" do |app|
+    #   app.middleware.insert_after ::ActionDispatch::DebugExceptions, ::SpreeRedirects::RedirectMiddleware
+    # end
 
     config.to_prepare {}
   end
